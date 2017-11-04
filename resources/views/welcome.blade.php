e150bc54471addb03c4f30f891fab758fbb1b29a
chenlei <568120554@qq.com>
2017-11-04 10:47:58 +0800

    one

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>seeYou</title>
+    <style>
+
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            margin: 0;
+        }
+        #content{
+            text-align: center;
+            vertical-align: middle;
+            width: 100%;
+            margin-top: 10%;
+            padding:0 0 15px 0;
+            font-size: 5vmin;
+        }
+        #link{
+            text-align: center;
+            font-size: 2.2vmax;
+        }
+        #link a{
+            text-decoration: none;
+            color: #636b6f;
+            margin:0 20%;
+        }
+    </style>
+</head>
+<body>
+    <div id="content">
+        凤兮凤兮归故乡，遨游四海求其凰。<br />
+        时未遇兮无所将，何悟今兮升斯堂！<br />
+        有艳淑女在闺房，室迩人遐毒我肠。<br />
+        何缘交颈为鸳鸯，胡颉颃兮共翱翔！<br />
+        凰兮凰兮从我栖，得托孳尾永为妃。<br />
+        交情通意心和谐，中夜相从知者谁？<br />
+        双翼俱起翻高飞，无感我思使余悲。<br />
+    </div>
+    <div id="link">
+        <a href="">SF</a>
+        <a href="">GITHUB</a>
+    </div>
+</body>
+</html>
